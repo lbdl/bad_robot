21964c144c5a7cefce33bcd72fc557d9b5b824e8
Tim Storey <timstorey@bite-software.com>
2018-08-23 21:56:45 +0100

    Split out functionality

---
+++ BadRobot/Utility/FileReader.swift
@@ -0,0 +1,39 @@
+//
+//  FileReader.swift
+//  BadRobot
+//
+//  Created by Timothy Storey on 23/08/2018.
+//  Copyright © 2018 BITE-Software. All rights reserved.
+//
+
+import Foundation
+
+struct InputReader {
+    
+    func getInput() -> String? {
+        print("Input path the Robot Command File", terminator: ": ")
+        if let inputLine = readLine() {
+            return inputLine
+        } else {
+            return nil
+        }
+    }
+    
+    func testPath(_ fPath: String ) throws {
+        let expandedString = fPath as NSString
+        let pathStr = String(expandedString.expandingTildeInPath)
+        
+        if !FileManager.default.fileExists(atPath: pathStr) {
+            throw FileReaderError.invalidPath(path: fPath)
+        }
+        
+        if !FileManager.default.isReadableFile(atPath: pathStr) {
+            throw FileReaderError.unreadableFile(path: pathStr)
+        }
+    }
+    
+}
+
+struct InputParser {
+    
+}
---
+++ BadRobot/Utility/FileReaderErrors.swift
@@ -0,0 +1,15 @@
+//
+//  FileReaderErrors.swift
+//  BadRobot
+//
+//  Created by Timothy Storey on 23/08/2018.
+//  Copyright © 2018 BITE-Software. All rights reserved.
+//
+
+import Foundation
+
+enum FileReaderError: Error {
+    case invalidPath(path: String)
+    case unreadableFile(path: String)
+    case invalidFormat
+}
