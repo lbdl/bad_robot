972ee1901f92dc48885a4b2c5a4de64cae1c463e
Tim Storey <timstorey@bite-software.com>
2018-08-23 21:56:12 +0100

    Restructure project

---
+++ BadRobot/Application/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BadRobot
+//
+//  Created by Timothy Storey on 23/08/2018.
+//  Copyright © 2018 BITE-Software. All rights reserved.
+//
+
+import Foundation
+
+let reader = InputReader()
+
